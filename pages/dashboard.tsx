f87549fc84b44498422b1bf23b89e6eb2992c704
deividsonsabino <deividsonsabino@outlook.com.br>
2022-03-27 19:00:20 -0300

    :sparkles: realizando logout automatico

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -7,6 +7,7 @@ export default function Dashboard() {
 
     useEffect(() => {
         api.get('/me').then(response => console.log(response))
+        .catch(err => console.log(err))
     },[])
 
     return(
