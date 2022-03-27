304e045b075cccf305a41ec994c35db9b15ec34b
deividsonsabino <deividsonsabino@outlook.com.br>
2022-03-27 14:53:19 -0300

    :sparkles: configurando cliente do axios

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode } from "react";
+import { api } from "../services/api";
 
 type SignInCredentials = {
     email: string;
@@ -20,11 +21,20 @@ export function AuthProvider({ children }: AuthProviderProps) {
     const isAuthenticated = false;
 
     async function signIn({ email, password }: SignInCredentials) {
-        console.log({ email, password });
+        try {
+            const repsonse = await api.post('sessions', {
+                email,
+                password
+            });
+            console.log(repsonse.data)
+        } catch (err) {
+            console.log(err);
+        }
+
     }
 
     return (
-        <AuthContext.Provider value={{signIn, isAuthenticated}}>
+        <AuthContext.Provider value={{ signIn, isAuthenticated }}>
             {children}
         </AuthContext.Provider>
     )
