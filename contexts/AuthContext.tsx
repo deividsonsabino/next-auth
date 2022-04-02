9cf130136e67e8928becdde802c3dbf448fd4e91
deividsonsabino <deividsonsabino@outlook.com.br>
2022-04-02 01:18:14 -0300

    :sparkles: broadcast de logout

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -16,7 +16,8 @@ type SignInCredentials = {
 }
 
 type AuthContextData = {
-    signIn(credentials: SignInCredentials): Promise<void>;
+    signIn: (credentials: SignInCredentials) => Promise<void>;
+    signOut: () => void;
     user: User;
     isAuthenticated: boolean;
 }
@@ -27,9 +28,13 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData)
 
-export function singOut() {
+let authChannel: BroadcastChannel
+
+export function signOut() {
     destroyCookie(undefined, 'nextauth.token')
     destroyCookie(undefined, 'nextauth.refreshToken')
+
+    authChannel.postMessage('signOut')
 
     Router.push('/')
 }
@@ -37,6 +42,25 @@ export function singOut() {
 export function AuthProvider({ children }: AuthProviderProps) {
     const [user, setUser] = useState<User>()
     const isAuthenticated = !!user;
+
+    useEffect(() => {
+        authChannel = new BroadcastChannel('auth')
+
+        authChannel.onmessage = (message) => {
+            switch (message.data) {
+                case 'signOut':
+                    signOut();
+                    authChannel.close();
+                    break;
+                case 'signIn':
+                    Router.push('/dashboard')
+                    authChannel.close();
+                    break;
+                default:
+                    break
+            }
+        }
+    }, [])
 
     useEffect(() => {
         const { 'nextauth.token': token } = parseCookies()
@@ -49,7 +73,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
                     setUser({ email, permissions, roles })
                 })
                 .catch(() => {
-                    singOut()
+                    signOut()
                 })
         }
 
@@ -82,14 +106,16 @@ export function AuthProvider({ children }: AuthProviderProps) {
             api.defaults.headers['Authorization'] = `Bearer ${token}`;
 
             Router.push('/dashboard');
+
+            authChannel.postMessage('signIn')
+
         } catch (err) {
             console.log(err);
         }
 
     }
-
     return (
-        <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+        <AuthContext.Provider value={{ signIn, signOut, isAuthenticated, user }}>
             {children}
         </AuthContext.Provider>
     )
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,20 +1,18 @@
 import { useContext, useEffect } from "react"
 import { Can } from "../components/Can"
 import { AuthContext } from "../contexts/AuthContext"
-import { api } from "../services/apiClient"
 import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard() {
-    const { user } = useContext(AuthContext)
+    const { user, signOut, isAuthenticated } = useContext(AuthContext)
+    console.log("aaaaaaaaa",signOut);
 
-    useEffect(() => {
-        api.get('/me').then(response => console.log(response))
-            .catch(err => console.log(err))
-    }, [])
 
     return (
         <>
             <h1>Dashboard {user?.email}</h1>
+
+            <button onClick={signOut}>Sign out</button>
 
             <Can permissions={['metrics.list']}>
                 <div>Métricas</div>
