1e0dcd052fbe726547870492845c130656067051
deividsonsabino <deividsonsabino@outlook.com.br>
2022-04-02 00:22:00 -0300

    :sparkles: validando permissões server

--- hooks/useCan.ts
+++ hooks/useCan.ts
@@ -1,37 +1,19 @@
 import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import { validateUserPermissions } from "../utils/validateUserPermission";
 
 type useCanParams = {
     permissions?: string[];
     roles?: string[];
 }
-export function useCan({ permissions, roles}:useCanParams) {
+export function useCan({ permissions, roles }: useCanParams) {
     const { user, isAuthenticated } = useContext(AuthContext)
 
     if (!isAuthenticated) {
         return false
     }
 
-    if (permissions?.length > 0) {
-        const hasAllPermissions = permissions?.every(permission => {
-            return user.permissions.includes(permission);
-        });
+    const userHasValidPermissions = validateUserPermissions({ user, permissions, roles })
+    return userHasValidPermissions;
 
-        if (!hasAllPermissions) {
-            return false
-        }
-    }
-
-    if (roles?.length > 0) {
-        const hasAllRoles = roles?.some(role => {
-            return user.permissions.includes(role);
-        });
-
-
-        if (!hasAllRoles) {
-            return false
-        }
-    }
-
-    return true
 }
--- utils/withSSRAuth.ts
+++ utils/withSSRAuth.ts
@@ -1,12 +1,20 @@
 import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
 import { destroyCookie, parseCookies } from "nookies";
 import { AuthTokenError } from "../services/errors/AuthTokenError";
+import decode from 'jwt-decode'
+import { validateUserPermissions } from "./validateUserPermission";
 
-export function withSSRAuth<P>(fn: GetServerSideProps<P>) {
+type WithSSRAuthOptions = {
+    permissions?: string[]
+    roles?: string[]
+}
+
+export function withSSRAuth<P>(fn: GetServerSideProps<P>, options?: WithSSRAuthOptions) {
     return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
         const cookies = parseCookies(ctx);
+        const token = cookies['nextauth.token'];
 
-        if (!cookies['nextauth.token']) {
+        if (!token) {
             return {
                 redirect: {
                     destination: '/',
@@ -14,6 +22,29 @@ export function withSSRAuth<P>(fn: GetServerSideProps<P>) {
                 }
             }
         }
+
+        console.log('aaa',options);
+        if (options) {
+            const user = decode<{ permissions: string[], roles: string[] }>(token);
+            const { permissions, roles } = options
+
+            const userHasValidPermissions = validateUserPermissions({
+                user,
+                permissions,
+                roles
+            })
+
+            if (!userHasValidPermissions) {
+                return {
+                    redirect: {
+                        destination: '/dashboard',
+                        permanent: false
+                    }
+                }
+            }
+
+        }
+
 
         try {
             return await fn(ctx)
